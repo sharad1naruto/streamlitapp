3f51f3fdbfe4294f27cf26c3c51113ecb02cc92d
R Sharad Vamsi <61038914+sharad1naruto@users.noreply.github.com>
2022-05-07 10:29:19 +0530

    Create st1.py

---
+++ Files/st1.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of AMAZON!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'AMZN'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2015-5-31', end='2021-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
